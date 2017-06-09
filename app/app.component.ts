01342497d9b87a766970747cc6c1172e27801239
Tanner Eustice <teustice@msn.com>
2017-06-09 09:28:48 -0700

    initial commit also, implement angular2 boilerplate

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Animal } from './animal.model';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Angular is good to go!</h1>
+  `
+})
+
+export class AppComponent {
+
+}
